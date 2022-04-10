8173f8fe3808577bd72fb1ec1e33b1b2902ff405
manishakumari020 <cqmanisha@gamil.com>
2022-04-10 15:57:11 +0000

    login-solution

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+
+import LogoutButton from '../LogoutButton'
+
+import './index.css'
+
+const About = () => (
+  <>
+    <Header />
+    <div className="about-container">
+      <h1>About Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+
+export default About
